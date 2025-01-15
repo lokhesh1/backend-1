464be8bdc68e35a8018f957c889c744204024dc8
lokhesh1 <lokheshmahesh0@gmail.com>
2025-01-15 17:59:01 +0530

    deploy5

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors({
-  origin:'http://localhost:3000',credentials:true
+  origin:'https://lokhesh1.github.io/frontend-1',credentials:true
 }));
 app.use(cookieParser());
 app.use(session({
